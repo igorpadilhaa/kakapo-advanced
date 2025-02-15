ebf6a54799a3b49e14ca0d6c562c47a0ae15b446
Igor Padilha <igor.padilha2004@gmail.com>
2025-02-15 12:22:26 -0400

    Atualiza o código do firmware

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,46 +1,103 @@
-// Constantes
-const int ntcPin = 34; // Pino conectado ao divisor de tensão
-const float seriesResistor = 10000.0; // Resistor fixo (em ohms)
-const float nominalResistance = 10000.0; // Resistência nominal do NTC a 25°C
-const float nominalTemperature = 25.0; // Temperatura nominal em graus Celsius
-const float bCoefficient = 3950.0; // Coeficiente Beta do NTC
-const float realSupplyVoltage = 3.3; // Tensão de alimentação real do ESP32 (em volts)
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+
+String serverName = "FIREBASE-URL";
+
+const int ntcPin = 34;
+const float seriesResistor = 10000.0;     // Resistor fixo (em ohms)
+const float nominalResistance = 10000.0;  // Resistência nominal do NTC a 25°C
+const float nominalTemperature = 25.0;    // Temperatura nominal em graus Celsius
+const float bCoefficient = 3950.0;        // Coeficiente Beta do NTC
+const float realSupplyVoltage = 3.3;      // Tensão de alimentação real do ESP32 (em volts)
+
+const int botaoPin = 33;
+const int ledPin = 25;
 
 void setup() {
-  Serial.begin(9600); // Inicializa a comunicação serial
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando-se ao Wi-Fi...");
+  }
+  
+  Serial.println("Conectado ao Wi-Fi!");
+  Serial.print("Endereço IP: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(ledPin, OUTPUT);
+
+  pinMode(ntcPin, INPUT);
+  pinMode(botaoPin, INPUT_PULLUP);
 }
 
 void loop() {
-  // Lê a tensão no pino analógico
+  static bool ligado = false;
+  
+  if (digitalRead(botaoPin) == LOW) {
+    ligado = !ligado;
+    digitalWrite(ledPin, ligado? HIGH : LOW);
+
+    Serial.println("Apertado!");
+    delay(500);
+  }
+  
+  if (ligado) {
+    float t = ler_temperatura();
+    registra_temperatura(t);
+  
+    Serial.print("Temperatura: ");
+    Serial.print(t);
+    Serial.println(" °C");
+    delay(10000);
+  }
+}
+
+float ler_temperatura() {
   int analogValue = analogRead(ntcPin);
   
-  // Ajuste para a tensão de alimentação real
   float voltage = analogValue * (realSupplyVoltage / 4095.0);
 
-  // Exibe a tensão medida no pino para depuração
   Serial.print("Tensão medida no pino: ");
   Serial.print(voltage);
   Serial.println(" V");
 
-  // Calcula a resistência do NTC
   float ntcResistance = seriesResistor * (realSupplyVoltage / voltage - 1);
 
-  // Exibe a resistência calculada para depuração
   Serial.print("Resistência do NTC: ");
   Serial.print(ntcResistance);
   Serial.println(" ohms");
 
-  // Fórmula para temperatura (em Kelvin)
   float temperatureKelvin = 1.0 / (1.0 / (nominalTemperature + 273.15) + 
                         (1.0 / bCoefficient) * log(ntcResistance / nominalResistance));
 
-  // Converte para Celsius
   float temperatureCelsius = temperatureKelvin - 273.15+6;
+  return temperatureCelsius;
+}
 
-  // Exibe a temperatura no monitor serial
-  Serial.print("Temperatura: ");
-  Serial.print(temperatureCelsius);
-  Serial.println(" °C");
+void registra_temperatura(float temperatura) {
+  StaticJsonDocument<200> jsonDoc;
+  jsonDoc["temperatura"] = temperatura;
+  
+  String jsonString;
+  serializeJson(jsonDoc, jsonString);
+  
+  HTTPClient http;
+  http.begin(serverName + "/pulseira/1.json" );
+  http.addHeader("Content-Type", "application/json");
 
-  delay(1000); // Atualiza a leitura a cada 1 segundo
+  int httpCode = http.PUT(jsonString);
+  Serial.print("Resposta: ");
+  Serial.println(httpCode);
+
+  String payload = http.getString();
+  Serial.println("- payload: ");
+  Serial.println(payload);
+  
+  http.end();
 }
