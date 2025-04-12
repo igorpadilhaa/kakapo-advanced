266473c29c6405fe7fe8464bfad68935227b933d
Igor Padilha <igor.padilha2004@gmail.com>
2025-04-12 11:59:26 -0400

    Update firmware

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,7 +5,7 @@
 const char* ssid = "SSID";
 const char* password = "PASSWORD";
 
-String serverName = "FIREBASE-URL";
+String serverName = "FIREBASE_URL";
 
 const int ntcPin = 34;
 const float seriesResistor = 10000.0;     // Resistor fixo (em ohms)
@@ -14,8 +14,11 @@ const float nominalTemperature = 25.0;    // Temperatura nominal em graus Celsiu
 const float bCoefficient = 3950.0;        // Coeficiente Beta do NTC
 const float realSupplyVoltage = 3.3;      // Tensão de alimentação real do ESP32 (em volts)
 
-const int botaoPin = 33;
-const int ledPin = 25;
+#define S0 4
+#define S1 7
+#define S2 8 
+#define S3 5
+#define OUT 22
 
 void setup() {
   Serial.begin(115200);
@@ -30,32 +33,31 @@ void setup() {
   Serial.print("Endereço IP: ");
   Serial.println(WiFi.localIP());
 
-  pinMode(ledPin, OUTPUT);
-
   pinMode(ntcPin, INPUT);
-  pinMode(botaoPin, INPUT_PULLUP);
+
+  pinMode(S0, OUTPUT);
+  digitalWrite(S0, HIGH);
+
+  pinMode(S1, OUTPUT);
+  digitalWrite(S1, LOW);
+
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(OUT, INPUT);
 }
 
 void loop() {
-  static bool ligado = false;
+  float t = ler_temperatura();
   
-  if (digitalRead(botaoPin) == LOW) {
-    ligado = !ligado;
-    digitalWrite(ledPin, ligado? HIGH : LOW);
+  unsigned int red, green, blue;
+  ler_cor(red, green, blue);
 
-    Serial.println("Apertado!");
-    delay(500);
-  }
-  
-  if (ligado) {
-    float t = ler_temperatura();
-    registra_temperatura(t);
-  
-    Serial.print("Temperatura: ");
-    Serial.print(t);
-    Serial.println(" °C");
-    delay(10000);
-  }
+  registrar(t, red, green, blue);
+
+  Serial.print("Temperatura: ");
+  Serial.print(t);
+  Serial.println(" °C");
+  delay(5000);
 }
 
 float ler_temperatura() {
@@ -80,15 +82,23 @@ float ler_temperatura() {
   return temperatureCelsius;
 }
 
-void registra_temperatura(float temperatura) {
+void registrar(float temperatura, unsigned int red, unsigned int green, unsigned blue) {
   StaticJsonDocument<200> jsonDoc;
+  StaticJsonDocument<200> arrayBuf;
+
+  JsonArray array = arrayBuf.to<JsonArray>();
+  array.add(red);
+  array.add(green);
+  array.add(blue);
+
   jsonDoc["temperatura"] = temperatura;
+  jsonDoc["rgb"] =  array;
   
   String jsonString;
   serializeJson(jsonDoc, jsonString);
   
   HTTPClient http;
-  http.begin(serverName + "/pulseira/1.json" );
+  http.begin(serverName + "/pulseira/73b06a10.json" );
   http.addHeader("Content-Type", "application/json");
 
   int httpCode = http.PUT(jsonString);
@@ -100,4 +110,25 @@ void registra_temperatura(float temperatura) {
   Serial.println(payload);
   
   http.end();
+}
+
+void ler_cor(unsigned int &red, unsigned int &green, unsigned int &blue) {
+  digitalWrite(S2, LOW); digitalWrite(S3, LOW);
+  red = pulseIn(OUT, LOW);
+
+  digitalWrite(S2, HIGH); digitalWrite(S3, HIGH);
+  green = pulseIn(OUT, LOW);
+
+  digitalWrite(S2, LOW); digitalWrite(S3, HIGH);
+  blue = pulseIn(OUT, LOW);
+
+  String json = "{\"red\":" + String(red) + ",\"green\":" + String(green) + ",\"blue\":" + String(blue) + "}";
+  Serial.println(json);
+
+  red = map(red, 3460, 743, 0, 255);
+  green = map(green, 2450, 1860, 0, 255);
+  blue = map(blue, 2830, 1250, 0, 255);
+
+  json = "{\"red\":" + String(red) + ",\"green\":" + String(green) + ",\"blue\":" + String(blue) + "}";
+  Serial.println(json);
 }
